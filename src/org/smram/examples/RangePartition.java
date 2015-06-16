19485dcc17f906c1866e894c998fc55f96160bd1
smram <smram@users.noreply.github.com>
2015-06-15 20:23:06 -0700

    Added interpolation search and tests for it. Refactored RangePartition into subclasses for each type of sarch algorithm

--- src/org/smram/examples/RangePartition.java
+++ src/org/smram/examples/RangePartition.java
@@ -7,22 +7,23 @@ import java.util.Arrays;
  *
  * @param <T>
  */
+
+//Given a choice, I would NOT write binary or interpolation search code 
+//from scratch, its tricky
 public class RangePartition<T extends Comparable<T>> {
-	
-	enum SearchAlgo { CUSTOM, JAVA, UNIF };
-	final private T[] pivots;
+
+	//enum SearchAlgo { CUSTOM, JAVA, INTERPOLATION };
+	final protected T[] pivots;
+	protected int numCompares;
 
 	public RangePartition(T[] pivots) {
 		this.pivots = pivots;
+		numCompares = 0;
 	}
-	
-	public int search(T searchKey, SearchAlgo searchAlgo) {
-		if (searchAlgo == SearchAlgo.CUSTOM)
-			return binarySearch(searchKey);
-		else if (searchAlgo == SearchAlgo.JAVA) 
-			return javaBinarySearch(searchKey);
-		else 
-			return unifDistBinarySearch(searchKey);
+
+	public int getNumCompares()
+	{
+		return numCompares;
 	}
 	
 	/**
@@ -32,14 +33,16 @@ public class RangePartition<T extends Comparable<T>> {
 	 * falls, using right-closed ranges e.g.
 	 * (-inf, pivot0], (pivot0, pivot1], (pivot1, pivot2]... (pivotN-1, inf)
 	 */
-	private int binarySearch(T searchKey) {
+	int search(T searchKey) {
+		resetNumCompares();
+
 		int hi = pivots.length;
 		int lo = 0;
 		int mid = lo; // only for case when it doesn't enter loop
-		
+
 		while(lo < hi) {
 			mid = getMidpointIndex(lo, hi); 
-			int cmp = searchKey.compareTo(pivots[mid]); // the last one might be a waste
+			int cmp = doCompare(searchKey, pivots[mid]); // the last one might be a waste
 			if (cmp == 0)
 				return mid; // searchKey = pivot, we assign to range = pivot indx
 			else if (cmp < 0) {
@@ -49,33 +52,107 @@ public class RangePartition<T extends Comparable<T>> {
 			}
 		}
 		// searchKey not found. so return the range that we must assign to
-		int cmp = searchKey.compareTo(pivots[mid]);
+		int cmp = doCompare(searchKey, pivots[mid]);
 		return (cmp < 0) ? mid : mid + 1;
 	}
-	
-	private int getMidpointIndex(int lo, int hi)
-	{
-		// (int) (lo+hi)/2.0 is recommended written this way to avoid overflow
+
+	private int doCompare(T p1, T p2) {
+		numCompares++;
+		return p1.compareTo(p2);
+	}
+
+	protected void resetNumCompares() {
+		numCompares = 0;
+	}
+
+	private int getMidpointIndex(int lo, int hi) {
+		// written this way instead of lo+hi/2 to avoid overflow
 		return lo + (int) ((hi-lo)/2.0); 
 	}
-	
+}
+
+class JavaRangePartition<T extends Comparable<T>> extends RangePartition<T> {
+	public JavaRangePartition(T[] pivots) {
+		super(pivots);
+	}
+
 	/**
 	 * @param searchKey
 	 * @return the index of the bucket into which searchKey will fall if buckets
 	 * are defined right-closed (except for last bucket which is both sides open)
 	 */
-	private int javaBinarySearch(T searchKey) {
+	@Override
+	int search(T searchKey) {
 		int indx = Arrays.binarySearch(pivots, searchKey);
 		if (indx > 0)
 			return indx; // intervals are right-closed => pivot indx=bucket array indx
 		else {// searchKey not found in pivots
-			// first part gets insertionIndx into the pivot array; see BinarySearch javadoc
-			// plus one converts from pivot array index into bucket array indx
+			// first part gets insertionIndx into pivot[]; see BinarySearch javadoc
+			// +1 converts from pivot array index into bucket array indx
 			return -(indx+1) + 1; 
 		}
 	}
-	
-	private int unifDistBinarySearch(T searchKey) {
-		throw new UnsupportedOperationException();
+}
+
+/**
+ * Interpolation search - fewer comparisons than binary search when search 
+ * space is uniform random distributed
+ * 
+ * @author smram
+ *
+ */
+// Given a choice, I wouldn't write this search code from scratch, its tricky. 
+// This is practice.
+// Here I try to recreate the search part from scratch from here:
+// http://data.linkedin.com/blog/2010/06/beating-binary-search
+class InterpolationRangePartition extends RangePartition<Integer> {
+	public InterpolationRangePartition(Integer[] pivots) {
+		super(pivots);
+	}
+
+	/**
+	 * To do interpolation search you need values to be numerical 
+	 * (max, min, max-min), not just notion of ranks. 
+	 * @param searchKey
+	 * @return index of the range that searchKey falls into
+	 */
+	@Override
+	int search(Integer searchKey) {
+		resetNumCompares();
+		
+		int lo = 0;
+		int hi = pivots.length - 1;
+		int min = Integer.MIN_VALUE;
+		int max = Integer.MAX_VALUE;
+		int splitter = lo;
+		
+		// if lo overshoots high... break
+		// if min or max overshoot/undershoot searchKey... break
+		while (lo < hi && searchKey >= min && searchKey <= max) {
+			long valueRange = (long)max-min; // needs to be long else overflow
+			final double interpolatedPct = 
+					((long)searchKey - min)/((double)valueRange);
+			final int loOffset = (int) ((hi - lo) * interpolatedPct);
+			splitter = lo + loOffset;
+			
+			numCompares++;
+			if (searchKey == pivots[splitter])
+				return splitter;
+			// actually, can splitter ever = pivots.length without lo=hi??
+			else if (splitter == 0 || splitter == pivots.length-1) 
+				break; // searched all pivots and not found
+			else if (searchKey < pivots[splitter]) {		
+				hi = splitter - 1;
+				max = pivots[hi];
+			} else {
+				lo = splitter + 1;
+				min = pivots[lo];
+			}
+		}
+		
+		// if we are here, lo >= hi, split point = lo
+		// TODO I think comparing to splitter works for all 3 stop conditions
+		numCompares++;
+		return (searchKey < pivots[splitter]) ? splitter : splitter+1;
 	}
 }
--- src/org/smram/examples/TestRangePartition.java
+++ src/org/smram/examples/TestRangePartition.java
@@ -3,12 +3,28 @@ package org.smram.examples;
 import java.util.Arrays;
 import java.util.Random;
 
-import org.smram.examples.RangePartition.SearchAlgo;
 import org.smram.stats.LongSummaryStats;
 
+/**
+ * Performance tests for {@link RangePartition} with integer keys
+ * @author smram
+ *
+ */
 public class TestRangePartition {
 	final private Random rand = new Random();
 
+	static class MoreMetrics<T extends Comparable<T>>
+	{
+		RangePartition<T> rp;
+		LongSummaryStats timeStats = new LongSummaryStats();
+		LongSummaryStats numCmpStats = new LongSummaryStats();
+		
+		MoreMetrics(RangePartition<T> rp)
+		{
+			this.rp = rp;
+		}
+	}
+	
 	public void perfTest(int numPivots, int numRuns)
 	{
 		final int NUM_SAMPLES_FOR_AVGTIME = 100;
@@ -17,38 +33,55 @@ public class TestRangePartition {
 		for (int i=0; i < pivots.length; i++)
 			pivots[i] = rand.nextInt();
 		Arrays.sort(pivots);
-		
-		RangePartition<Integer> rp = new RangePartition<Integer>(pivots);
-		LongSummaryStats timeStatsCustom = new LongSummaryStats();
-		LongSummaryStats timeStatsJava = new LongSummaryStats();
 
 		// test: same pivots, different search algos
-		// measure avg time for numRuns runs
+		// measure average time for numRuns runs
+		MoreMetrics<Integer> bsp = new MoreMetrics<Integer>(
+				new RangePartition<Integer>(pivots));
+		MoreMetrics<Integer> jsp = new MoreMetrics<Integer>(
+				new JavaRangePartition<Integer>(pivots));
+		MoreMetrics<Integer> isp = new MoreMetrics<Integer>(
+				new InterpolationRangePartition(pivots));
+		
 		for (int j=0; j < NUM_SAMPLES_FOR_AVGTIME; j++)
 		{
-			long startBS = System.currentTimeMillis();
-			for (int i = 0; i < numRuns; i++)
-				rp.search(rand.nextInt(), SearchAlgo.CUSTOM);
-			long timeCustomBS = System.currentTimeMillis() - startBS;
-			timeStatsCustom.update(timeCustomBS);
+			runSearch(bsp, numRuns);
 			
-			startBS = System.currentTimeMillis();
-			for (int i = 0; i < numRuns; i++)
-				rp.search(rand.nextInt(), SearchAlgo.JAVA);
-			long timeJavaBS = System.currentTimeMillis() - startBS;
-			timeStatsJava.update(timeJavaBS);
+			runSearch(jsp, numRuns);
 			
-			//System.out.println(timeCustomBS + "," + timeJavaBS);
+			runSearch(isp, numRuns);
 		}
-		System.out.println(String.format("Time (ms) for %d searches, Cust BinSearch: %s",
-				numRuns, timeStatsCustom));
-		System.out.println(String.format("Time (ms) for %d searches, Java BinSearch: %s",
-				numRuns, timeStatsJava));
 		
-		// TODO some 1-sided test here comparing two SummaryStats
-		// Compare the two means.. assuming different variances... 
+		System.out.println(String.format("Time (ms) for %d searches, Cust BinSearch: %s",
+				numRuns, bsp.timeStats));
+		System.out.println(String.format("Time (ms) for %d searches, Java BinSearch: %s",
+				numRuns, jsp.timeStats));
+		System.out.println(String.format("Time (ms) for %d searches, Interpol Search: %s",
+				numRuns, isp.timeStats));
+		
+		System.out.println(String.format("#Cmp (ms) for %d searches, Cust BinSearch: %s",
+				numRuns, bsp.numCmpStats));
+		System.out.println(String.format("#Cmp (ms) for %d searches, Interpol Search: %s",
+				numRuns, isp.numCmpStats));
+		
+		// TODO some 1-sided test comparing SummaryStats means 
+		// Compare the means.. assume different variances... 
 	}
 	
+	private MoreMetrics<Integer> runSearch(MoreMetrics<Integer> met, int numRuns)
+	{
+		long startBS = System.currentTimeMillis();
+		for (int i = 0; i < numRuns; i++)
+			met.rp.search(rand.nextInt());
+		long time = System.currentTimeMillis() - startBS;
+		
+		met.timeStats.update(time);
+		met.numCmpStats.update(met.rp.getNumCompares());
+		
+		return met;
+	}
+
+	///////////////////////////////////////////////////////////////////////////
 	public static void main(String[] args) {
 		if (args.length < 2)
 			throw new IllegalArgumentException("Too few arguments. Usage: TestRangePartition <numPivots> <numRuns>");
@@ -63,5 +96,13 @@ public class TestRangePartition {
 }
 
 // Result: my observation is the the first few numRuns samples take much longer
-// more for customBinSearch than javaBinSearch -- is it hot spot optimizing?
-// reproduce by running with numPivots = 1000, numRuns=10000, NUM_SAMPLES_FOR_AVGTIME=100
+//         more for customBinSearch than javaBinSearch -- is it hot spot optimizing?
+//         reproduce by running with numPivots = 1000, numRuns=10000, NUM_SAMPLES_FOR_AVGTIME=100
+// Result: Interpolation search does expected fewer comparisons than binary search: O(log(log(n))
+//         However, it isn't always faster in elapsed time, needs optimization.
+// Example result:
+//Time (ms) for 100 searches, Cust BinSearch: num=100, avg=0.240000, sd=1.146977, min=0, max=11
+//Time (ms) for 100 searches, Java BinSearch: num=100, avg=0.230000, sd=1.369620, min=0, max=13
+//Time (ms) for 100 searches, Interpol Search: num=100, avg=0.150000, sd=0.500000, min=0, max=4
+//#Cmp (ms) for 100 searches, Cust BinSearch: num=100, avg=4.000000, sd=0.000000, min=4, max=4
+//#Cmp (ms) for 100 searches, Interpol Search: num=100, avg=2.390000, sd=0.665074, min=2, max=4
